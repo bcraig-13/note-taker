87af0fae70946ea8277795714818605ca35365c0
Brandon Craig <twilightraven13@yahoo.com>
2021-01-17 01:42:34 -0800

    Added 'app.get/spi/notes' that returns res.json. Added app.post for new notes. May need to be fixed later.

--- public/server.js
+++ public/server.js
@@ -15,9 +15,18 @@ app.get("/notes", (req, res) => {
 });
 
 // GET /api/notes - Should read the db.json file and return all saved notes as JSON.
-
+app.get("/api/notes", (req, res) => {
+    return res.json(characters);
+});
 // POST /api/notes - Should receive a new note to save on the request body, add it to the db.json file, and then return the new note to the client.
-
+app.post("/api/notes", (req, res) => {
+    const newnote = req.body;
+  
+    console.log(newnote);
+    notes.push(newnote);
+    res.json(newnote);
+    //===Still need to create a notes array var
+  });
 // DELETE /api/notes/:id - Should receive a query parameter containing the id of a note to delete. This means you'll need to find a way to give each note a 
 // unique id when it's saved. In order to delete a note, you'll need to read all notes from the db.json file, remove the note with the given id property, and 
 // then rewrite the notes to the db.json file.
