0b2db04c184209c674c6d4d5f209e9d678ac1bcb
Brandon Craig <thegamingraven13@gmail.com>
2021-01-18 21:32:44 -0800

    Set a const to require fs. Created 'const database'. Created 'app.post'. Created 'app.delete'.

--- server.js
+++ server.js
@@ -2,11 +2,13 @@ const express = require("express");
 const path = require("path");
 const app = express();
 const PORT = 3000;
-const notes = [];
+const fs = require("fs");
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
+
+const database = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "/public/index.html"));
@@ -16,28 +18,38 @@ app.get("/notes", (req, res) => {
   res.sendFile(path.join(__dirname, "/public/notes.html"));
 });
 
-// GET /api/notes - Should read the db.json file and return all saved notes as JSON.
 app.get("/api/notes", (req, res) => {
-    return res.json(notes);
+  return res.json(database);
 });
-// POST /api/notes - Should receive a new note to save on the request body, add it to the db.json file, and then return the new note to the client.
-app.post("/api/notes", (req, res) => {
-    const newnote = req.body;
-    
-    //===MOVES NOTE TO ARRAY===
-    // notes.push(newnote);
-    // res.json(newnote);
-    // console.log(newnote);
-    
-});
-// DELETE /api/notes/:id - Should receive a query parameter containing the id of a note to delete. This means you'll need to find a way to give each note a 
-// unique id when it's saved. In order to delete a note, you'll need to read all notes from the db.json file, remove the note with the given id property, and 
-// then rewrite the notes to the db.json file.
 
-//===Takes user to index if nothing is added to url. Keep last?
-// app.get("*", (req, res) => {
-//     res.sendFile(path.join(__dirname, "index.html"));
-//   });
+app.post("/api/notes", (req, res) => {
+  let finalDb = database.length - 1;
+  let id;
+
+  if (finalDb < 0) {
+    id = 1;
+  } else {
+    id = database[finalDb]["id"] + 1;
+  }
+
+  database.push({ id, ...req.body });
+  res.json(database);
+  console.log(database);
+});
+
+app.delete("/api/notes/:id", (req, res) => {
+  var noteToDelete = database.find(
+    ({ id }) => id === JSON.parse(req.params.id)
+  );
+
+  database.splice(database.indexOf(noteToDelete), 1);
+  console.log(database);
+  res.end();
+});
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
 
 app.listen(PORT, () => {
   console.log(`App listening on PORT: http://localhost:${PORT}`);
